3323f1565af7d13e6c5f8cffe20b53a184d70b4e
Manikandan Baskar <mani111355@gmail.com>
2026-09-24 13:52:14 +0530

    Alignment: normalize source overlaps before topology alignment

--- src/Services/Alignment/AlignmentService.psm1
+++ src/Services/Alignment/AlignmentService.psm1
@@ -9,14 +9,36 @@ function Invoke-AlignmentPreview {
         [Parameter(Mandatory=$true)][double]$Gap
     )
 
-    $analysis = Get-PbiLayoutAnalysis -PageSnapshot $PageSnapshot `
+    # First classify the current page, then normalize any unintended
+    # active-visual overlaps in memory. No PBIR file is written at this stage.
+    $initialAnalysis = Get-PbiLayoutAnalysis -PageSnapshot $PageSnapshot `
         -MinimumTolerance ([double]$Config.layout.minimumTolerance) `
         -MaximumTolerance ([double]$Config.layout.maximumTolerance)
 
+    $overlapNormalization = Resolve-PbiSnapshotOverlaps -PageSnapshot $PageSnapshot -InitialAnalysis $initialAnalysis -Margin $Margin -Gap $Gap
+
+    $analysisSnapshot = $overlapNormalization.Snapshot
+    $analysis = if ($overlapNormalization.HadOverlaps) {
+        Get-PbiLayoutAnalysis -PageSnapshot $analysisSnapshot `
+            -MinimumTolerance ([double]$Config.layout.minimumTolerance) `
+            -MaximumTolerance ([double]$Config.layout.maximumTolerance)
+    }
+    else {
+        $initialAnalysis
+    }
+
     $layout = Get-SmartPbiLayout -Analysis $analysis -Margin $Margin -Gap $Gap
+    $layout | Add-Member -MemberType NoteProperty -Name PreNormalizationOverlapPairs -Value ([int]$overlapNormalization.InitialOverlapPairCount) -Force
+    $layout | Add-Member -MemberType NoteProperty -Name PreNormalizationMovedCount -Value ([int]$overlapNormalization.MovedCount) -Force
+    $layout | Add-Member -MemberType NoteProperty -Name PreNormalizationResizedCount -Value ([int]$overlapNormalization.ResizedCount) -Force
+    $layout | Add-Member -MemberType NoteProperty -Name PreNormalizationRemainingOverlapPairs -Value ([int]$overlapNormalization.RemainingOverlapPairCount) -Force
+    $layout | Add-Member -MemberType NoteProperty -Name PreNormalizationCompactFallback -Value ([bool]$overlapNormalization.UsedCompactFallback) -Force
+
     $validation = Test-PbiLayout -Layout $layout
 
     [pscustomobject]@{
+        InitialAnalysis = $initialAnalysis
+        OverlapNormalization = $overlapNormalization
         Analysis = $analysis
         Layout = $layout
         Validation = $validation
