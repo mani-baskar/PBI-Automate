ad260131eee9065f663d3c20169b5dbd8255360a
Manikandan Baskar <mani111355@gmail.com>
2026-09-24 08:14:07 +0530

    V1: protect canvas-sized background visuals from smart alignment

--- src/Core/LayoutAnalyzer.psm1
+++ src/Core/LayoutAnalyzer.psm1
@@ -26,30 +26,133 @@ function Get-ClusterStarts {
     return @($clusters | Sort-Object Center)
 }
 
+function Test-CanvasBackgroundLike {
+    param(
+        [Parameter(Mandatory=$true)]$Visual,
+        [Parameter(Mandatory=$true)][double]$PageWidth,
+        [Parameter(Mandatory=$true)][double]$PageHeight
+    )
+
+    if ($PageWidth -le 0 -or $PageHeight -le 0) { return $false }
+
+    $widthRatio = [double]$Visual.Width / $PageWidth
+    $heightRatio = [double]$Visual.Height / $PageHeight
+
+    return ($widthRatio -ge 0.90 -and $heightRatio -ge 0.90)
+}
+
 function Get-PbiLayoutAnalysis {
     [CmdletBinding()]
-    param([Parameter(Mandatory=$true)]$PageSnapshot,[double]$MinimumTolerance=3,[double]$MaximumTolerance=24)
-    $visuals=@($PageSnapshot.Visuals)
-    if ($visuals.Count -eq 0) { throw 'The selected page does not contain supported visual containers.' }
+    param(
+        [Parameter(Mandatory=$true)]$PageSnapshot,
+        [double]$MinimumTolerance=3,
+        [double]$MaximumTolerance=24
+    )
+
+    $allVisuals=@($PageSnapshot.Visuals)
+    if ($allVisuals.Count -eq 0) { throw 'The selected page does not contain supported visual containers.' }
+
+    $locked=@()
+    $visuals=@()
+
+    foreach ($visual in $allVisuals) {
+        if (Test-CanvasBackgroundLike -Visual $visual -PageWidth ([double]$PageSnapshot.Width) -PageHeight ([double]$PageSnapshot.Height)) {
+            $locked += $visual
+        } else {
+            $visuals += $visual
+        }
+    }
+
+    if ($visuals.Count -eq 0) {
+        throw 'No layout-managed visuals remain after protecting canvas-sized background/decorative objects.'
+    }
+
     $medianWidth=Get-MedianValue -Values ([double[]]@($visuals | ForEach-Object { $_.Width }))
     $medianHeight=Get-MedianValue -Values ([double[]]@($visuals | ForEach-Object { $_.Height }))
     $xTolerance=[Math]::Min($MaximumTolerance,[Math]::Max($MinimumTolerance,$medianWidth*0.08))
     $yTolerance=[Math]::Min($MaximumTolerance,[Math]::Max($MinimumTolerance,$medianHeight*0.08))
     $columnClusters=@(Get-ClusterStarts -Values ([double[]]@($visuals | ForEach-Object { $_.X })) -Tolerance $xTolerance)
     $rowClusters=@(Get-ClusterStarts -Values ([double[]]@($visuals | ForEach-Object { $_.Y })) -Tolerance $yTolerance)
+
     $items=@()
+
     foreach ($v in $visuals) {
         $col=0; $bestX=[double]::MaxValue
-        for ($i=0; $i -lt $columnClusters.Count; $i++) { $d=[Math]::Abs($v.X-$columnClusters[$i].Center); if ($d -lt $bestX) { $bestX=$d; $col=$i } }
+        for ($i=0; $i -lt $columnClusters.Count; $i++) {
+            $d=[Math]::Abs($v.X-$columnClusters[$i].Center)
+            if ($d -lt $bestX) { $bestX=$d; $col=$i }
+        }
+
         $row=0; $bestY=[double]::MaxValue
-        for ($i=0; $i -lt $rowClusters.Count; $i++) { $d=[Math]::Abs($v.Y-$rowClusters[$i].Center); if ($d -lt $bestY) { $bestY=$d; $row=$i } }
-        $colSpan=1; if ($medianWidth -gt 0) { $colSpan=[Math]::Max(1,[int][Math]::Round($v.Width/$medianWidth)) }
-        $rowSpan=1; if ($medianHeight -gt 0) { $rowSpan=[Math]::Max(1,[int][Math]::Round($v.Height/$medianHeight)) }
+        for ($i=0; $i -lt $rowClusters.Count; $i++) {
+            $d=[Math]::Abs($v.Y-$rowClusters[$i].Center)
+            if ($d -lt $bestY) { $bestY=$d; $row=$i }
+        }
+
+        $colSpan=1
+        if ($medianWidth -gt 0) { $colSpan=[Math]::Max(1,[int][Math]::Round($v.Width/$medianWidth)) }
+
+        $rowSpan=1
+        if ($medianHeight -gt 0) { $rowSpan=[Math]::Max(1,[int][Math]::Round($v.Height/$medianHeight)) }
+
         if ($columnClusters.Count -gt 0) { $colSpan=[Math]::Min($colSpan,$columnClusters.Count-$col) }
         if ($rowClusters.Count -gt 0) { $rowSpan=[Math]::Min($rowSpan,$rowClusters.Count-$row) }
-        $items += [pscustomobject]@{ Id=$v.Id; VisualType=$v.VisualType; FilePath=$v.FilePath; SourceHash=$v.FileHash; X=$v.X; Y=$v.Y; Width=$v.Width; Height=$v.Height; Column=$col; Row=$row; ColumnSpan=[Math]::Max(1,$colSpan); RowSpan=[Math]::Max(1,$rowSpan); IsLarge=(($v.Width -gt ($medianWidth*1.6)) -or ($v.Height -gt ($medianHeight*1.6))) }
+
+        $items += [pscustomobject]@{
+            Id=$v.Id
+            VisualType=$v.VisualType
+            FilePath=$v.FilePath
+            SourceHash=$v.FileHash
+            X=$v.X
+            Y=$v.Y
+            Width=$v.Width
+            Height=$v.Height
+            Column=$col
+            Row=$row
+            ColumnSpan=[Math]::Max(1,$colSpan)
+            RowSpan=[Math]::Max(1,$rowSpan)
+            IsLarge=(($v.Width -gt ($medianWidth*1.6)) -or ($v.Height -gt ($medianHeight*1.6)))
+            IsLocked=$false
+            AllowOverlap=$false
+        }
     }
-    [pscustomobject]@{ PageWidth=[double]$PageSnapshot.Width; PageHeight=[double]$PageSnapshot.Height; VisualCount=$visuals.Count; MedianWidth=$medianWidth; MedianHeight=$medianHeight; ColumnTolerance=$xTolerance; RowTolerance=$yTolerance; ColumnCount=[Math]::Max(1,$columnClusters.Count); RowCount=[Math]::Max(1,$rowClusters.Count); ColumnStarts=@($columnClusters | ForEach-Object { $_.Center }); RowStarts=@($rowClusters | ForEach-Object { $_.Center }); Items=$items }
+
+    foreach ($v in $locked) {
+        $items += [pscustomobject]@{
+            Id=$v.Id
+            VisualType=$v.VisualType
+            FilePath=$v.FilePath
+            SourceHash=$v.FileHash
+            X=$v.X
+            Y=$v.Y
+            Width=$v.Width
+            Height=$v.Height
+            Column=0
+            Row=0
+            ColumnSpan=1
+            RowSpan=1
+            IsLarge=$true
+            IsLocked=$true
+            AllowOverlap=$true
+        }
+    }
+
+    [pscustomobject]@{
+        PageWidth=[double]$PageSnapshot.Width
+        PageHeight=[double]$PageSnapshot.Height
+        VisualCount=$allVisuals.Count
+        ManagedVisualCount=$visuals.Count
+        LockedVisualCount=$locked.Count
+        MedianWidth=$medianWidth
+        MedianHeight=$medianHeight
+        ColumnTolerance=$xTolerance
+        RowTolerance=$yTolerance
+        ColumnCount=[Math]::Max(1,$columnClusters.Count)
+        RowCount=[Math]::Max(1,$rowClusters.Count)
+        ColumnStarts=@($columnClusters | ForEach-Object { $_.Center })
+        RowStarts=@($rowClusters | ForEach-Object { $_.Center })
+        Items=$items
+    }
 }
 
 Export-ModuleMember -Function Get-PbiLayoutAnalysis
